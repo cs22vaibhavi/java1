7c21f6c39d0b6dc6c430bf1ba59ff4553acc6ea9
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-06-15 06:02:14 -0700

    Add multiple classes with print statements in Java

---
+++ new.java
@@ -0,0 +1,52 @@
+class FirstClass{
+    public static void main(String args[]){
+        System.out.println("hello world");
+    }
+}
+
+class SecondClass{
+    public static void main(String args[]){
+        System.out.println("Vaibhavi R Shanbhag");
+    }
+}
+
+//println
+class ThirdClass{
+    public static void main(String args[]){
+        System.out.println("My name is vaibhavi");
+        System.out.println("I am from kumta");
+    }
+
+}
+
+//print
+class FourthClass{
+    public static void main(String args[]){
+        System.out.print("My name is vaibhavi");
+        System.out.print("I am from kumta");
+    }
+
+}
+
+//print by \n
+class FifthClass{
+    public static void main(String args[]){
+        System.out.print("My name is vaibhavi\n");
+        System.out.print("I am from kumta\n");
+        System.out.println("Hi\nGood morning");
+    }
+
+}
+
+class SixthClass{
+    public static void main(String args[]){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+    }
+
+}
+
