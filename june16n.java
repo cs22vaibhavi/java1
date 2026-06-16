2757915075f22312b89daf2b303ab22b6ea70203
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-06-16 05:30:15 -0700

    Create variables1 class to read user input
    Added a Java program to read and print an integer input.

---
+++ june16n.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class variables1 {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        //input
+        Scanner sc=new Scanner(System.in);
+        //String name=sc.next();       //gives only one word
+        //String name=sc.nextLine();    //gives full name
+        //nextInt()
+        int age=sc.nextInt();
+        System.out.println(age);
+    }
+    
+}
+
+
+
