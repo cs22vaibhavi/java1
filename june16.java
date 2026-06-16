b054485eb844150ad8fc38c1f52e263e96ba56ba
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-06-16 05:29:09 -0700

    Implement basic arithmetic operations in variables class
    Corrected arithmetic operations and added a second class for demonstration.

---
+++ june16.java
@@ -0,0 +1,40 @@
+//variables
+public class variables {
+     public static void main(String args[]){
+        int a=10;
+        int b=25;
+        int sum=a+b;
+        System.out.println(sum);
+
+        int diff=a-b;
+        System.out.println(diff);
+
+        int mul=a*b;
+        System.out.println(mul);
+
+        int div=a/b;
+        System.out.println(div);
+    }
+}
+//varible2
+class variable2{
+    public static void main(String args[]){
+
+        int a=100;
+        int b=250;
+        int sum=a+b;
+        System.out.println(sum);
+
+        int diff=a-b;
+        System.out.println(diff);
+
+        int mul=a*b;
+        System.out.println(mul);
+
+        int div=a/b;
+        System.out.println(div);
+    }
+}
+
+
+
