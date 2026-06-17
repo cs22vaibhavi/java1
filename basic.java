db18eb9ac8d9fd6b458f82ed75327252bbfaa3ab
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-06-17 03:58:23 -0700

    Add HelloWorld and AddNumbers classes

---
+++ basic.java
@@ -0,0 +1,15 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+class AddNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
