ca909c93ecfcf42aeab48c48df4a33a8827e9aaf
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-06-18 04:04:40 -0700

    Add condition checking for even and multiple of three

---
+++ june18part2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class condition1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+
+        if(x%2==0){
+            System.out.println("Even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
+
+class condition3{
+    public static void main(String[] args) {
+        int x=50;
+
+        if(x%3==0){
+            System.out.println("multiply");
+        }else{
+            System.out.println("not multiply");
+        }
+    }
+}
