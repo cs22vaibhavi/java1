048df5ee16b76bf3be30a6de85fd82aff8955b3e
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-06-20 23:12:18 -0700

    Add basic calculator functionality in calculator.java

---
+++ calculator.java
@@ -0,0 +1,12 @@
+public class calculator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        System.out.println("Addition="+(a+b));
+        System.out.println("Substraction="+(a-b));
+        System.out.println("mutiplication="+(a*b));
+        System.out.println("division="+(a/b));
+
+    }
+}
