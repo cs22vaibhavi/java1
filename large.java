3bea6f313ba5787895a27c9908456df326cd915a
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-07-07 17:30:30 +0530

    Add Largest class to find the largest of three numbers

---
+++ large.java
@@ -0,0 +1,12 @@
+public class Largest {
+    public static void main(String[] args) {
+        int a = 15, b = 25, c = 10;
+
+        if (a >= b && a >= c)
+            System.out.println("Largest = " + a);
+        else if (b >= a && b >= c)
+            System.out.println("Largest = " + b);
+        else
+            System.out.println("Largest = " + c);
+    }
+}
