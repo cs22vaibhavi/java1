ead0703adcdb24fc9249b70fa5e51cb1085bb9c9
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-07-08 19:57:58 +0530

    Add PrintDetails class to display user information

---
+++ simple.java
@@ -0,0 +1,8 @@
+public class PrintDetails {
+    public static void main(String[] args) {
+        System.out.println("Name: Vaibhavi");
+        System.out.println("Course: BCA");
+        System.out.println("Semester: 6");
+        System.out.println("City: Mysuru");
+    }
+}
