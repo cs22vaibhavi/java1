04506483e52b8eca748348ad8cafa2e868a370eb
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-07-14 18:29:52 +0530

    Create PrintNames class to display names
    This class prints a list of names to the console.

---
+++ name.java
@@ -0,0 +1,14 @@
+public class PrintNames {
+    public static void main(String[] args) {
+        System.out.println("Vaibhavi");
+        System.out.println("Rahul");
+        System.out.println("Priya");
+        System.out.println("Amit");
+        System.out.println("Sneha");
+        System.out.println("Kiran");
+        System.out.println("Rohan");
+        System.out.println("Anjali");
+        System.out.println("Neha");
+        System.out.println("Arjun");
+    }
+}
