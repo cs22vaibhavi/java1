d7dda2641891f9d0bde95aea9c282dcde6a1d14c
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-07-16 19:16:28 +0530

    Add CompareNumbers class to compare two integers

---
+++ equal.java
@@ -0,0 +1,10 @@
+public class CompareNumbers {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+
+        boolean isEqual = (a == b);
+
+        System.out.println(isEqual);
+    }
+}
