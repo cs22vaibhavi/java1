18dcca234511df96a4d6beaccd5e86305ed36a88
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-07-18 20:33:45 +0530

    Create StringAdd class to concatenate strings

---
+++ string.java
@@ -0,0 +1,10 @@
+public class StringAdd {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "World";
+
+        String result = s1 + " " + s2;
+
+        System.out.println("Result: " + result);
+    }
+}
