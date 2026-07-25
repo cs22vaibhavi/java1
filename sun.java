9ec2d04464b5db5d74318dcddc891b99a932ea4a
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-07-25 18:53:49 +0530

    Add SunGlow class with main method

---
+++ sun.java
@@ -0,0 +1,12 @@
+public class SunGlow {
+    public static void main(String[] args) {
+
+        boolean sunGlow = true;
+
+        if (sunGlow) {
+            System.out.println("The sun is glowing.");
+        } else {
+            System.out.println("The sun is not glowing.");
+        }
+    }
+}
