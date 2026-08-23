7ea55463f5e33da499ca6a4785047f522cebcb87
Vaibhavi Radhakrishna Shanbhag <165009122+cs22vaibhavi@users.noreply.github.com>
2026-08-23 20:07:48 +0530

    Add Average class to calculate average of three numbers

---
+++ Average.java
@@ -0,0 +1,11 @@
+public class Average {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int average = (a + b + c) / 3;
+
+        System.out.println("Average = " + average);
+    }
+}
